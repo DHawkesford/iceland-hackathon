1fee164a725101bcd2293fc4204892216ac3c973
Bukola Jimoh <bukola.jimoh@yahoo.co.uk>
2022-10-28 15:32:13 +0100

    completed styling

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,18 +1,21 @@
 import "./productList.scss";
-import productPaymentSplit from './ProductTile/productPaymentSplit/productPaymentSplit.js';
 
-function ProductList({basket, setBasket}) {
+function ProductList({ basket, setBasket }) {
   return (
     <div className="container">
-      {basket.map((item, index) => (
-        <li className="item" key={item.price}>
-          <img src={item.image} alt="product-item-pic"></img>
-          <p>{item.product}</p>
-          <p>Quantity: {item.quantity}</p>
-          <p>Price: £{item.price}</p>
-          <productPaymentSplit index={index}></productPaymentSplit>
-        </li>
-      ))}
+      <ul>
+        {basket.map((item, index) => (
+          <li className="item" key={item.price}>
+            <img src={item.image} alt="product-item-pic"></img>
+            <div className="text-container">
+              <p>{item.product}</p>
+              <p>Quantity{item.quantity}</p>
+              <p>Price: £{item.price}</p>
+            </div>
+            <productPaymentSplit index={index}></productPaymentSplit>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
