ab37428e972079eeac8b4f7dcd46eaaa40bb5731
danielhawkesford <daniel.hawkesford@gmail.com>
2022-11-01 14:29:54 +0000

    updated Totals styling

--- src/components/Totals/Totals.js
+++ src/components/Totals/Totals.js
@@ -26,16 +26,16 @@ function Totals({paymentMethods, basket}) {
     <div className="total-container">
         <h2>3. Confirm totals and checkout</h2>
         {paymentMethods.map(method => 
-        <div className='payment-total'>
+        <div className='payment-method'>
           <span className='payment-type'>{method}</span>
           <span className='payment-quantity'>{totals[method].quantity} items</span>
-          <span className='payment-total'>{totals[method].total}</span>
+          <span className='payment-total'>£{totals[method].total}</span>
         </div>
         )}
-        <div className='payment-total'>
+        <div className='payment-method-total'>
           <span className='payment-type'>Total</span>
           <span className='payment-quantity'>{totals.total.quantity} items</span>
-          <span>{totals.total.total}</span>
+          <span>£{totals.total.total}</span>
         </div>
     </div>
   );
